480e3055b66faf8dc409c9a1b50593a4084bd43b
springhack <springhack@live.cn>
2020-10-28 01:02:46 +0800

    Initial Commit

---
+++ floating demo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  floating demo
+//
+//  Created by SpringHack on 2020/10/28.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ floating demo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  floating demo
+//
+//  Created by SpringHack on 2020/10/28.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
