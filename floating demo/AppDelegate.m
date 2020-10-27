30d6915becad29ba66269c1399efeeb8ad1241ee
springhack <springhack@live.cn>
2020-10-28 01:27:44 +0800

    init demo

--- floating demo/AppDelegate.m
+++ floating demo/AppDelegate.m
@@ -9,19 +9,58 @@
 
 @interface AppDelegate ()
 
-
 @end
 
-@implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+@implementation AppDelegate {
+  NSMutableArray* windows_;
 }
 
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+  // Insert code here to initialize your application
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+  // Init windows array
+  windows_ = [[NSMutableArray alloc] init];
+
+  // Create windows for every screen
+  for (NSScreen* screen in [NSScreen screens]) {
+    // Window size
+    NSRect frame = [screen frame];
+    frame.origin.x += 200;
+    frame.origin.y += 200;
+    frame.size.width /= 2;
+    frame.size.height /= 2;
+
+    // Create window
+    NSWindow* window = [[NSPanel alloc]
+        initWithContentRect:frame
+                  styleMask:NSWindowStyleMaskTitled |
+                            NSWindowStyleMaskNonactivatingPanel  // the most important mask
+                    backing:NSBackingStoreBuffered
+                      defer:YES
+                     screen:screen];
+
+    // Set window space behavior
+    [window setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces |
+                                  NSWindowCollectionBehaviorFullScreenAuxiliary];
+
+    // Set window level
+    [window setLevel:CGShieldingWindowLevel()];
+
+    // Some demo code to make window transparent
+    [window setOpaque:NO];
+    [window setBackgroundColor:[NSColor colorWithDeviceRed:0.0 green:1.0 blue:0.0 alpha:0.1]];
+
+    // Make window on the top
+    [window orderFrontRegardless];
+
+    // Add to array to keep reference
+    [windows_ addObject:window];
+  }
 }
 
+- (void)applicationWillTerminate:(NSNotification*)aNotification {
+  // Insert code here to tear down your application
+  NSLog(@"%@", windows_);
+}
 
 @end
--- floating demo/main.m
+++ floating demo/main.m
@@ -6,10 +6,15 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-    }
-    return NSApplicationMain(argc, argv);
+int main(int argc, const char* argv[]) {
+  @autoreleasepool {
+    // Setup code that might create autoreleased objects goes here.
+    AppDelegate* delegate = [[AppDelegate alloc] init];
+    NSApplication* application = [NSApplication sharedApplication];
+    [application setDelegate:delegate];
+    [application run];
+  }
+  return EXIT_SUCCESS;
 }
